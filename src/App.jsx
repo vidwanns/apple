80470b41f63588ea13580bf1f23b69bc5f2dd62b
vidwan <163408880+vidwanns@users.noreply.github.com>
2024-09-18 00:29:28 +0530

    macbook font

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,12 @@ import MacContainer from "./MacContainer.jsx";
 
 const App = () => {
   return (
-  <div className= "w-full h-screen bg-blue-300">
+  <div className= "w-full h-screen">
+    <div className="absolute text-white top-32 left-1/2 -translate-x-1/2 font-family:'Helvetica Now Display'">
+    <h3 className="text-7xl tracking-tighter font-[700]">
+      Macbook pro.
+    </h3>
+    </div>
   <Canvas camera={{fov:12, position: [0, -10, 220] }}>
     <OrbitControls/>
     <Environment files={['https://dl.polyhaven.org/file/ph-assets/HDRIs/exr/4k/studio_small_09_4k.exr',
