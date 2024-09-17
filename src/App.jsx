b533edd04d24ee1f913204016bcc9f15c98d3e0a
vidwan <163408880+vidwanns@users.noreply.github.com>
2024-09-17 22:33:05 +0530

    display on screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,24 @@
 import {Canvas} from "@react-three/fiber";
 import React from "react";
 import "./style.css"
-import {Environment, OrbitControls} from "@react-three/drei";
+import {Environment, OrbitControls, ScrollControls} from "@react-three/drei";
 import MacContainer from "./MacContainer.jsx";
 
 
 const App = () => {
-  return <Canvas camera={{fov:12, position: [0, -10, 220] }}>
+  return (
+  <div className= "w-full h-screen bg-blue-300">
+  <Canvas camera={{fov:12, position: [0, -10, 220] }}>
     <OrbitControls/>
     <Environment files={['https://dl.polyhaven.org/file/ph-assets/HDRIs/exr/4k/studio_small_09_4k.exr',
-    ]}/>
+    ]}
+    />
+    <ScrollControls pages={3}>
     <MacContainer/>
+    </ScrollControls>
   </Canvas>
-    
-  
+  </div>
+  );
 };
 
 export default App;
